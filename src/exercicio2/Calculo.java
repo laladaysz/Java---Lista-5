6d69b197a900d67f1f57589e6b5b9a15a0338e7a
laladaysz <lauraleandra.dias@gmail.com>
2024-06-13 16:35:29 -0300

    :sparkles: feat: new functions

--- src/exercicio2/Calculo.java
+++ src/exercicio2/Calculo.java
@@ -1,4 +1,19 @@
 package exercicio2;
 
 public class Calculo {
+
+    public double INSS (double salario) {
+        return salario * 0.20;
+    }
+
+    public double IRPF (double salario) {
+        return salario * 0.10;
+    }
+
+    public double Unimed (double salario) {
+        return salario * 0.05;
+    }
+       
+
+
 }
